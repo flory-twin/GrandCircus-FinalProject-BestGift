09e1bb464c4d67537e153be8be2645c75328a20b
flory-twin <flory.twin.2@gmail.com>
2019-12-05 17:02:40 -0500

    Initial commit.

---
+++ src/main/java/co/grandcircus/bestgift/BestgiftApplication.java
@@ -0,0 +1,13 @@
+package co.grandcircus.bestgift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestgiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestgiftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/grandcircus/bestgift/BestgiftApplicationTests.java
@@ -0,0 +1,13 @@
+package co.grandcircus.bestgift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestgiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
