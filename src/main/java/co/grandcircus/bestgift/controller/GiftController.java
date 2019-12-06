129ede4df1eee604672684c084f2cf771169bd54
Bryan-Be <bryan.d.byrd@gmail.com>
2019-12-06 10:45:07 -0500

    Adds model for image response; adds test controller to verify image fetch.

--- src/main/java/co/grandcircus/bestgift/controller/GiftController.java
+++ src/main/java/co/grandcircus/bestgift/controller/GiftController.java
@@ -7,6 +7,7 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
 import co.grandcircus.bestgift.models.GiftResult;
+import co.grandcircus.bestgift.models.Image;
 
 @Controller
 public class GiftController {
@@ -29,4 +30,26 @@ public class GiftController {
 		mv.addObject("giftresult", result.getResults().get(0));
 		return mv;
 	}
+	
+	@RequestMapping("/image")
+	public ModelAndView giftImages(String listing_id) {
+		ModelAndView mv = new ModelAndView("TestOutPut");
+		
+		String url = "https://openapi.etsy.com/v2/listings/" + listing_id + "/images?api_key=" + etsyKey;
+		
+//		String result = rt.getForObject(url, String.class);
+		Image result = rt.getForObject(url, Image.class);
+		
+		
+		mv.addObject("i", result);
+		return mv;
+		
+	}
+	
+	@RequestMapping("/image/newSearch")
+	public ModelAndView giftImagesNoUrl() {
+		return new ModelAndView("TestOutPut");
+	}
+	
+	
 }
