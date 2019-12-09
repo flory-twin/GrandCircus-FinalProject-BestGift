7ffd31c312bb89b8a34712e992331b1e7813eca0
Kevin Chung <chungkev7@gmail.com>
2019-12-09 16:34:52 -0500

    Added Keyword Repository and Keyword table identifiers

---
+++ src/main/java/co/grandcircus/bestgift/jparepos/KeywordRepository.java
@@ -0,0 +1,9 @@
+package co.grandcircus.bestgift.jparepos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import co.grandcircus.bestgift.search.Keyword;
+
+public interface KeywordRepository extends JpaRepository<Keyword, Integer> {
+
+}
