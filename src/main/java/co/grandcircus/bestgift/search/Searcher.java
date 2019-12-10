d77492c3979a1784cdadccb75626f6eae697727b
Kevin Chung <chungkev7@gmail.com>
2019-12-10 11:21:27 -0500

    Added Search expression methodology, refactored current search methods

--- src/main/java/co/grandcircus/bestgift/search/Searcher.java
+++ src/main/java/co/grandcircus/bestgift/search/Searcher.java
@@ -7,9 +7,9 @@ import java.util.List;
 import co.grandcircus.bestgift.models.Gift;
 import co.grandcircus.bestgift.models.GiftResult;
 
-public class Searcher {
+public abstract class Searcher<T> {
 
-	private List<Gift> lastRoundOfGifts; 
+	protected List<Gift> lastRoundOfGifts; 
 	
 	public Searcher(List<Gift> giftsToSearchAmong) {
 		lastRoundOfGifts = giftsToSearchAmong;
@@ -23,23 +23,7 @@ public class Searcher {
 	 * @param k
 	 * @return
 	 */
-	public List<Gift> findMatchingGifts(Keyword k) {
-		List<Gift> nextRoundOfGifts = new ArrayList<>();
-		
-		// Go through the list of available gifts; 
-		for (Gift nextGift : lastRoundOfGifts) {
-			GiftMatcher searcher = new GiftMatcher(nextGift);
-			// If the next available Gift matches,
-			if (searcher.matches(k))
-			{
-				// Add that Gift to the list.
-				nextRoundOfGifts.add(nextGift);
-			}
-		}
-		
-		// We're finished, so send the results back.
-		return nextRoundOfGifts;
-	}
+	public abstract List<Gift> findMatchingGifts(T t);
 	
 	/**
 	 * Like findMatchingGifts(Keyword), but limits the number of max results.
@@ -48,84 +32,7 @@ public class Searcher {
 	 * @param numberToFind
 	 * @return
 	 */
-	public List<Gift> findMatchingGifts(Keyword k, int maxNumberToFind) {
-		List<Gift> nextRoundOfGifts = new ArrayList<>();
-		
-		boolean moreGiftsLeftToSearch = (lastRoundOfGifts.size() > 0);
-		// Until we're out of Gifts or we have numberToFind matching Gifts,
-		while (maxNumberToFind > 0 && moreGiftsLeftToSearch) {
-			// Go through the list of available gifts; 
-			for (Gift nextGift : lastRoundOfGifts) {
-				GiftMatcher searcher = new GiftMatcher(nextGift);
-				// If the next available Gift matches,
-				if (searcher.matches(k))
-				{
-					// Add that Gift to the list.
-					nextRoundOfGifts.add(nextGift);
-					// Since we've checked one more Gift off the list, reduce the number we still need to find.
-					maxNumberToFind--;
-				}
-			}
-		}
-		
-		// We're finished, so send the results back.
-		return nextRoundOfGifts;
-	}
-	
-	// What we know will be true about the SearchExpression (because of how we set up the constructors):
-	//  1. The first keyword will ALWAYS be set.
-	//  2. If an Operator is set, then either (2a) the second keyword is set, or (2b) the interior search expression is set.
-	public List<Gift> findMatchingGifts(SearchExpression s, int maxNumberToFind) {
-		if (s.getOperator() == null) {
-			// Case 1
-			//  To evaluate this case, just check whether the keyword matches.
-			return findMatchingGifts(s.getKeyword1());
-		} else {
-			// Case 2
-			//  To evaluate this case:
-			//   Get a list of Gifts matching Keyword 1.
-			//   Get a second list of Gifts--either by (a) matching on Keyword 2, or (b) by matching the SearchExpression stored inside this object.
-			//   Apply the Operator to these two lists.
-			
-			// A note on limiting: I'm going to pull numberToFind^2 members from each of the child lists. That should give us enough .
-			int maxKidsToMatch = (int) Math.pow((double) maxNumberToFind, 2.0);
-			List<Gift> giftsMatchingKeyword1 = findMatchingGifts(s.getKeyword1(), maxKidsToMatch);
-			List<Gift> secondListOfGifts = null;
-			
-			if (s.getKeyword2() != null) {
-				// Case 2a
-				//   Get a list of Gifts matching keyword 2.
-				secondListOfGifts = findMatchingGifts(
-						new SearchExpression(s.getKeyword2()), maxKidsToMatch);
-			} else {
-				// Case 2b
-				//  Evaluate the BaseExpression to get a list of results.
-				secondListOfGifts = findMatchingGifts(
-						s.getBaseExpression(), maxNumberToFind);
-			}
-			
-			//   For either variant of Case 2, apply the Operator to these two lists.
-			if (s.getOperator() == Operator.OR) {
-				// If the operator is OR, return:
-				//  - Results matching Keyword 1
-				//  - Results matching Keyword 2
-				//  - Results matching both keywords		
-				
-				// Append.
-				List<Gift> allGifts = giftsMatchingKeyword1;
-				allGifts.addAll(secondListOfGifts);
-				
-				// Remove IDs by duplicate.
-				Searcher.removeDuplicatesByListingId(allGifts);
-				
-				// Return list.
-				return allGifts;
-			}
-		}
-		
-		// If we're here, we've impossibly fallen through all the other logic.
-		return null;
-	}
+	public abstract List<Gift> findMatchingGifts(T t, int maxNumberToFind);
 
 	public static void sortGiftListByListingId(List<Gift> toBeSorted) {
 		toBeSorted.sort(
